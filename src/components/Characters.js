0380deed4e09087716779bd2395998e536a95cc6
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-12 12:44:18 -0500

    Finalize & clean up

---
+++ src/components/Characters.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { Character } from "./Character";
+
+export const Characters = () => {
+  const [page, setPage] = useState(2);
+  const fetchCharacters = async ({ queryKey }) => {
+    const res = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+    );
+    return res.json();
+  };
+
+  const { data, status, isPreviousData } = useQuery(
+    ["characters", page],
+    fetchCharacters,
+    {
+      keepPreviousData: true,
+    }
+  );
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "error") {
+    return <div>Error</div>;
+  }
+  return (
+    <div className="characters">
+      {data.results.map((character) => (
+        <Character key={character.id} character={character} />
+      ))}
+      <div>
+        <button disabled={!data.info.prev} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <button
+          disabled={isPreviousData && !data.info.next}
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
